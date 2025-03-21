9910ee8bdeba918d9bcf9b08a550a79d0a2a7698
Alexandr Khakana <Kalashiq.org@gmail.com>
2025-03-21 23:27:12 +0200

    feat: enhance birthday sorting with upcoming year section

--- src/components/Search/UserList.tsx
+++ src/components/Search/UserList.tsx
@@ -24,6 +24,41 @@ const UserItem = styled.div`
   }
 `;
 
+const YearDivider = styled.div`
+  position: relative;
+  margin: 38px 0;
+  text-align: center;
+
+  &::before, &::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    width: 508px;
+    height: 1px;
+    background-color: #C3C3C6;
+    transform: rotate(180deg);
+  }
+
+  &::before {
+    right: 50%;
+    margin-right: 24px;
+  }
+
+  &::after {
+    left: 50%;
+    margin-left: 24px;
+  }
+`;
+
+const YearLabel = styled.span`
+  background-color: white;
+  padding: 0 40px;
+  color: #97979B;
+  font-size: 15px;
+  position: relative;
+  z-index: 1;
+`;
+
 const Avatar = styled.img`
   width: 72px;
   height: 72px;
@@ -64,23 +99,55 @@ interface UserListProps {
 }
 
 const UserList: React.FC<UserListProps> = ({ users, sortType }) => {
-  const sortedUsers = useMemo(() => {
-    if (!sortType) return users;
-    if (sortType === "alphabet") {
-      return sortUsersByAlphabet(users);
-    } else {
-      return sortUsersByBirthday(users);
+  const groupedUsers = useMemo(() => {
+    if (sortType !== "birthday") {
+      return { currentYear: sortUsersByAlphabet(users), nextYear: [] };
     }
+
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const sortedUsers = sortUsersByBirthday(users);
+
+    return sortedUsers.reduce(
+      (acc, user) => {
+        const [, month, day] = user.birthday.split("-");
+        const birthDate = new Date(currentYear, parseInt(month) - 1, parseInt(day));
+
+        if (birthDate < currentDate) {
+          acc.nextYear.push(user);
+        } else {
+          acc.currentYear.push(user);
+        }
+        return acc;
+      },
+      { currentYear: [] as User[], nextYear: [] as User[] }
+    );
   }, [users, sortType]);
+
+  if (sortType !== "birthday") {
+    return (
+      <ListContainer>
+        {groupedUsers.currentYear.map((user) => (
+          <UserItem key={user.id}>
+            <Avatar src={user.avatarUrl} alt={`${user.firstName} ${user.lastName}`} />
+            <Content>
+              <NameContainer>
+                <Name>{`${user.firstName} ${user.lastName}`}</Name>
+                <Tag>{user.userTag}</Tag>
+              </NameContainer>
+              <Department>{user.department}</Department>
+            </Content>
+          </UserItem>
+        ))}
+      </ListContainer>
+    );
+  }
 
   return (
     <ListContainer>
-      {sortedUsers.map((user) => (
+      {groupedUsers.currentYear.map((user) => (
         <UserItem key={user.id}>
-          <Avatar
-            src={user.avatarUrl}
-            alt={`${user.firstName} ${user.lastName}`}
-          />
+          <Avatar src={user.avatarUrl} alt={`${user.firstName} ${user.lastName}`} />
           <Content>
             <NameContainer>
               <Name>{`${user.firstName} ${user.lastName}`}</Name>
@@ -90,6 +157,25 @@ const UserList: React.FC<UserListProps> = ({ users, sortType }) => {
           </Content>
         </UserItem>
       ))}
+      {groupedUsers.nextYear.length > 0 && (
+        <>
+          <YearDivider>
+            <YearLabel>{new Date().getFullYear() + 1}</YearLabel>
+          </YearDivider>
+          {groupedUsers.nextYear.map((user) => (
+            <UserItem key={user.id}>
+              <Avatar src={user.avatarUrl} alt={`${user.firstName} ${user.lastName}`} />
+              <Content>
+                <NameContainer>
+                  <Name>{`${user.firstName} ${user.lastName}`}</Name>
+                  <Tag>{user.userTag}</Tag>
+                </NameContainer>
+                <Department>{user.department}</Department>
+              </Content>
+            </UserItem>
+          ))}
+        </>
+      )}
     </ListContainer>
   );
 };
--- src/utils/sorting.ts
+++ src/utils/sorting.ts
@@ -9,20 +9,22 @@ export const sortUsersByAlphabet = (users: User[]): User[] => {
 };
 
 export const sortUsersByBirthday = (users: User[]): User[] => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
   return [...users].sort((a, b) => {
     const [, monthA, dayA] = a.birthday.split("-");
     const [, monthB, dayB] = b.birthday.split("-");
 
-    const dateA = new Date(
-      new Date().getFullYear(),
-      parseInt(monthA) - 1,
-      parseInt(dayA)
-    );
-    const dateB = new Date(
-      new Date().getFullYear(),
-      parseInt(monthB) - 1,
-      parseInt(dayB)
-    );
+    const dateA = new Date(currentYear, parseInt(monthA) - 1, parseInt(dayA));
+    const dateB = new Date(currentYear, parseInt(monthB) - 1, parseInt(dayB));
+
+    if (dateA < currentDate) {
+      dateA.setFullYear(currentYear + 1);
+    }
+    if (dateB < currentDate) {
+      dateB.setFullYear(currentYear + 1);
+    }
 
     return dateA.getTime() - dateB.getTime();
   });
