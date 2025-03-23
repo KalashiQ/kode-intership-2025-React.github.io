0a191bcfd791bffc287b5b8f597a079ecf51686c
Alexandr Khakana <Kalashiq.org@gmail.com>
2025-03-23 19:49:19 +0200

    build: add dist folder for deployment

---
+++ src/shared/types/types.ts
@@ -0,0 +1,14 @@
+export type TabType = 
+  | "all" 
+  | "design" 
+  | "analytics" 
+  | "management" 
+  | "ios" 
+  | "android" 
+  | "frontend" 
+  | "backend" 
+  | "support" 
+  | "qa" 
+  | "back_office" 
+  | "hr" 
+  | "pr"; 
